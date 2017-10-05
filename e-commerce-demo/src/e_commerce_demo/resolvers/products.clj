c38e20a6c6f39d2fe30395a76a34d5096f3ac8e6
Tiago Luchini <tiagoluchini@Luchini-Tiago-Pro.local>
2017-10-04 23:32:55 -0400

    Wrap product in a Hystrix command

--- e-commerce-demo/src/e_commerce_demo/resolvers/products.clj
+++ e-commerce-demo/src/e_commerce_demo/resolvers/products.clj
@@ -1,11 +1,17 @@
 (ns e-commerce-demo.resolvers.products
   (:require [camel-snake-kebab.core :refer [->camelCaseKeyword]]
             [camel-snake-kebab.extras :refer [transform-keys]]
+            [com.netflix.hystrix.core :refer [defcommand]]
             [e-commerce-demo.query :as query]))
+
+(defcommand fetch-products
+  {:hystrix/fallback-fn (fn [db q pred delayer] {:products/products []})}
+  [db q pred delayer]
+  (query/query db q {:products/products pred} delayer))
 
 (defn resolver [db delayer]
   (fn [context arguments value]
     (let [q `[{:products/products ~(query/selectors context)}]
           pred (if (nil? arguments) identity #(= (:db/id %) (:byId arguments)))
-          {:keys [products/products]} (query/query db q {:products/products pred} delayer)]
+          {:keys [products/products]} (fetch-products db q pred delayer)]
       (transform-keys ->camelCaseKeyword products))))
